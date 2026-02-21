55b84c2590fe4702a85a8bfae42c060b33123f1e
perkashlal <parkashlal450@gmail.com>
2026-02-21 02:44:10 +0100

    implemented findAll to return empty list

--- src/main/java/com/examples/employee/repository/mongo/EmployeeMongoRepository.java
+++ src/main/java/com/examples/employee/repository/mongo/EmployeeMongoRepository.java
@@ -3,11 +3,11 @@ package com.examples.employee.repository.mongo;
 import java.util.List;
 import com.examples.employee.model.Employee;
 import com.examples.employee.repository.EmployeeRepository;
-
+import java.util.Collections;
 public class EmployeeMongoRepository implements EmployeeRepository {
 
 	@Override
 	public List<Employee> findAll() {
-		return null;
+		return Collections.emptyList();
 	}
 }
