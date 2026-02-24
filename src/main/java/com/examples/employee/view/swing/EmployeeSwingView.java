c333e4258772ea499599636e41853602fb4cdbb0
perkashlal <parkashlal450@gmail.com>
2026-02-24 02:54:29 +0100

    feat: implement Add button enabling logic (GREEN)

--- src/main/java/com/examples/employee/view/swing/EmployeeSwingView.java
+++ src/main/java/com/examples/employee/view/swing/EmployeeSwingView.java
@@ -1,6 +1,8 @@
 package com.examples.employee.view.swing;
 
 import java.awt.FlowLayout;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,38 +13,54 @@ import com.examples.employee.controller.EmployeeController;
 
 public class EmployeeSwingView extends JFrame {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+	private JTextField idTextBox;
+	private JTextField nameTextBox;
+	private JButton btnAdd;
 
-    public EmployeeSwingView() {
-        setLayout(new FlowLayout());
+	public EmployeeSwingView() {
+		setLayout(new FlowLayout());
 
-        add(new JLabel("id"));
-        JTextField idTextBox = new JTextField(10);
-        idTextBox.setName("idTextBox");
-        add(idTextBox);
+		add(new JLabel("id"));
+		idTextBox = new JTextField(10);
+		idTextBox.setName("idTextBox");
+		add(idTextBox);
 
-        add(new JLabel("name"));
-        JTextField nameTextBox = new JTextField(10);
-        nameTextBox.setName("nameTextBox");
-        add(nameTextBox);
+		add(new JLabel("name"));
+		nameTextBox = new JTextField(10);
+		nameTextBox.setName("nameTextBox");
+		add(nameTextBox);
 
-        JButton btnAdd = new JButton("Add");
-        btnAdd.setEnabled(false);
-        add(btnAdd);
+		btnAdd = new JButton("Add");
+		btnAdd.setEnabled(false);
+		add(btnAdd);
 
-        JList<String> employeeList = new JList<>(new DefaultListModel<>());
-        employeeList.setName("employeeList");
-        add(employeeList);
+		JList<String> employeeList = new JList<>(new DefaultListModel<>());
+		employeeList.setName("employeeList");
+		add(employeeList);
 
-        JButton btnDelete = new JButton("Delete Selected");
-        btnDelete.setEnabled(false);
-        add(btnDelete);
+		JButton btnDelete = new JButton("Delete Selected");
+		btnDelete.setEnabled(false);
+		add(btnDelete);
 
-        JLabel errorMessageLabel = new JLabel("");
-        errorMessageLabel.setName("errorMessageLabel");
-        add(errorMessageLabel);
-    }
+		JLabel errorMessageLabel = new JLabel("");
+		errorMessageLabel.setName("errorMessageLabel");
+		add(errorMessageLabel);
 
-    public void setEmployeeController(EmployeeController employeeController) {
-    }
+		KeyAdapter btnAddEnabler = new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				btnAdd.setEnabled(
+					!idTextBox.getText().trim().isEmpty() &&
+					!nameTextBox.getText().trim().isEmpty()
+				);
+			}
+		};
+
+		idTextBox.addKeyListener(btnAddEnabler);
+		nameTextBox.addKeyListener(btnAddEnabler);
+	}
+
+	public void setEmployeeController(EmployeeController employeeController) {
+	}
 }
