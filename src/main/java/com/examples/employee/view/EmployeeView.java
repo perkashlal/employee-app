9cf297586478a6368a7f8f6ac792641cbba804f1
perkashlal <parkashlal450@gmail.com>
2026-02-28 03:01:39 +0100

    Refactor(View): Synchronize EmployeeView interface with updated controller requirements

--- src/main/java/com/examples/employee/view/EmployeeView.java
+++ src/main/java/com/examples/employee/view/EmployeeView.java
@@ -6,10 +6,9 @@ import com.examples.employee.model.Employee;
 public interface EmployeeView {
     void showAllEmployees(List<Employee> employees);
 
-    void employeeAdded(Employee employee);
 
     void employeeRemoved(Employee employee);
-
+    void employeeAdded(Employee employee);
     void showError(String message, Employee employee);
     void showErrorEmployeeNotFound(String message, Employee employee);
 }
